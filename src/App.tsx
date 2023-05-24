be3f941bc5622ab213cf0bd62a2ffe7b6cdb8b60
federico2277 <101802997+federico2277@users.noreply.github.com>
2023-05-24 10:36:50 +0200

    2 commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,117 +1,61 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import logo from './logo.svg';
 import {useState} from "react";
 import './App.css';
 import { isSet } from 'util/types';
+import { loadavg } from 'os';
 
-const data = [
-    { name: "Anom", age: 19, gender: "Male" },
-    { name: "Megha", age: 19, gender: "Female" },
-    { name: "Subham", age: 25, gender: "Male"},
-    { name: "Alessandro", age: 19, gender: "Male"},
-    { name: "Mario", age: 25, gender: "Male"},
-    { name: "Giovanni", age: 28, gender: "Male"},
-  ]
+
 
   function App(){
+    const data = [{ id:0 ,nome: "Anom", cognome: "Anom", data_nascita: "2005-02-17",codice_fiscale:"RAFWEJ647DSVS",id_classe:0}]
+    const [students,setStudents]=useState(data);
+    const [loading, setLoading] = useState(true);
+    const [error, setError] = useState(null);
 
-    const [students, setStudents] = useState(data);
-    const [showForm, setShowForm] = useState(false);
-    const [nome, setName] = useState("");
-    const [eta, setAge] = useState("");
-    const [sesso, setGender] = useState("");
 
-    const [editFormData, setEditFormData] = useState({
-      nome: "",
-      eta: "",
-      sesso: "",
-      
-    });
 
-    function elimina(el: String){
-      
-        setStudents(students.filter(obj => obj.name !== el));
-    }
+  
+    useEffect(() => {
+      fetch(`http://localhost:8080/API/student`)
+        .then((response) => response.json())
+        .then((actualData) => {
+          setStudents(actualData.records)
+        });
+    }, []);
 
-    function aggiungi(){
-      let newStudentsArray = students;
-      newStudentsArray.push({name: nome, age: parseInt(eta), gender: sesso});
-      setStudents(newStudentsArray);
-      button();
-    }
 
-    function button(){
-      if(showForm==false){
-        setShowForm(true);
-      }
-      else{
-        setShowForm(false);
-      }
-    }
-
-    function modify(){
-      
-      if(showForm==false){
-        setShowForm(true);
-      }
-      else{
-        setShowForm(false);
-        
-      }
-      
-      
-    }
 
     return (
         <div className="App">
           <center>
           <table border = {2}>
             <tr>
-              <th>Name</th>
-              <th>Age</th>
-              <th>Gender</th>
+              <th>Nome</th>
+              <th>Cognome</th>
+              <th>Codice Fiscale</th>
+              <th>Data Nascita</th>
               <th>Elimina</th>
               <th>Modifica</th>
             </tr>
-            {students.map((val, key) => {
+            
+            {students.map((val,id) => {
               return (
-                <tr key={key}>
-                  <td>{val.name}</td>
-                  <td>{val.age}</td>
-                  <td>{val.gender}</td>
-                  <td><button onClick={() => elimina(val.name)}>Delete</button></td>
-                  <td><button onClick={() => modify()}>Modify</button></td>
+                <tr key={id}>
+                  <td>{val.nome}</td>
+                  <td>{val.cognome}</td>
+                  <td>{val.codice_fiscale}</td>
+                  <td>{val.data_nascita}</td>
+
+
                 </tr>
               )
             })}
+            
+            
           </table>
-          <button onClick={() => button()}>Inserisci</button>
-          {showForm &&
-          <form> 
+          
 
-            <br/>
-            <br/>
-            <input type="text" name="name" onChange={e => setName(e.target.value)}/>
-            
-            <br/>
-            <br/>
-            <input type="text" name="age" onChange={e => setAge(e.target.value)}/>
-            
-            <br/>
-            <br/>
-            <select name="gender" onChange={e => setGender(e.target.value)}>
-
-              <option value="Male">Male</option>
-              <option value="Female">Female</option>
-              
-            </select>
-
-            <br/>
-            <br/>
-            <button onClick={()=> aggiungi()}>SALVA</button>
-
-          </form>
-          }
           </center>
         </div>
       );
